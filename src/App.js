0fcd53edbf6ec10b3e7b58d9190c3a7518577433
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-18 15:08:20 -0400

    Add Home to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Home from './pages/Home';
 
 function App() {
-  return null;
+  return (
+    <main>
+      <Home />
+    </main>
+  );
 }
 
 export default App;
