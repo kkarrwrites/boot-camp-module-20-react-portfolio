34d7bd7dc0d56d4269fd64f6ce01ac35148d3665
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-19 15:18:42 -0400

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
-function Navigation() {
-  return null;
+function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <nav>
+      <h1>
+        <a href="/" onClick={() => handlePageChange('About')}>
+          K. Karr
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href="/" onClick={() => handlePageChange('About')}>
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#portfolio" onClick={() => handlePageChange('Portfolio')}>
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a href="#resume" onClick={() => handlePageChange('Resume')}>
+            Resume
+          </a>
+        </li>
+        <li>
+          <a href="#contact" onClick={() => handlePageChange('Contact')}>
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
