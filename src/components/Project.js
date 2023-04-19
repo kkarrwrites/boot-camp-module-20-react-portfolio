897a69d26df38d1373c69fee703b3e4a4dc88069
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-19 16:39:11 -0400

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-function Project() {
+function Project({ imageUrl, title, gitHubUrl, deploymentUrl }) {
   return (
     <article>
-      <img src="#"></img>
+      <img src={imageUrl} alt={title} />
+      <div>
+        <h3>{title}</h3>
+        <p>
+          <a href={gitHubUrl} target="_blank">
+            GitHub
+          </a>{' '}
+          | <a href={deploymentUrl} target="_blank"></a>Deployment
+        </p>
+      </div>
     </article>
   );
 }
