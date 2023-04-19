b31de233784f2a0bce363aa480390ae64bb0e833
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-19 15:18:30 -0400

    Update Resume.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
 function Resume() {
-  return null;
+  return (
+    <section>
+      <h2>Resume</h2>
+      <p>
+        Download my <a href="#">resume</a>.
+      </p>
+      <h3>Skills</h3>
+      <ul>
+        <li>HTML CSS</li>
+        <li>SCSS (Sass)</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>React.js</li>
+        <li>NoSQL</li>
+        <li>MongoDB</li>
+        <li> Mongoose ODM</li>
+        <li> SQL</li>
+        <li> MySQL</li>
+        <li> Sequelize ORM</li>
+        <li> Git</li>
+      </ul>
+    </section>
+  );
 }
 
 export default Resume;
