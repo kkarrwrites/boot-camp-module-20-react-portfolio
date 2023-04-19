49271f6b626e01b78e0f2aec891c5cf6cb5f6575
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-19 15:18:19 -0400

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function About() {
-  return null;
+  return (
+    <section>
+      <h2>About</h2>
+      <p>Hello! I'm K. Karr, a copy editor turned frontend web developer.</p>
+      <p>
+        I got my start tinkering with HTML for Expage, GeoCities, and
+        LiveJournal in high school.
+      </p>
+      <p>
+        After collecting a few degrees, living abroad for two years, and
+        cultivating an eye for detail working as a copy editor for a decade, I
+        returned to web development out of a strong desire to learn more about
+        the technologies I use every day.
+      </p>
+      <p>
+        I specialize in HTML, CSS, JavaScript, Node.js, Express.js, React, and
+        MongoDB.
+      </p>
+    </section>
+  );
 }
 
 export default About;
