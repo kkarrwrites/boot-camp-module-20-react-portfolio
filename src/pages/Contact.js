7a0607dd5bab3e00a5a23788b3cb06ac07df837f
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-19 15:18:33 -0400

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 function Contact() {
-  return null;
+  return (
+    <section>
+      <h2>Contact</h2>
+      <form>
+        <label htmlFor="name" required>
+          Name
+        </label>
+        <br />
+        <input id="name" type="text" />
+        <br />
+        <label htmlFor="email">Email</label>
+        <br />
+        <input
+          id="email"
+          /* type="email" */
+          pattern="/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/"
+          required
+        />
+        <br />
+        <label htmlFor="message" required>
+          Message
+        </label>
+        <br />
+        <textarea id="message" type="text" />
+        <br />
+        <button type="submit" value="Submit">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default Contact;
