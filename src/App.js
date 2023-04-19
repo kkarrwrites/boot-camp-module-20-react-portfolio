f40ca27e983547b10195cd5e868ab1fb8337244e
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-19 15:18:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import Home from './pages/Home';
+import React, { useState } from 'react';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+import './assets/style.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <main>
-      <Home />
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      <Header />
+      {renderPage()}
+      <Footer />
     </main>
   );
 }
