3650dcc4b5cf2450f1426d3f71d61ca390da1c48
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-19 15:18:47 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function Footer() {
-  return null;
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="https://github.com/kkarrwrites">GitHub</a>
+        </li>
+        <li>
+          <a href="#">LinkedIn</a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;
