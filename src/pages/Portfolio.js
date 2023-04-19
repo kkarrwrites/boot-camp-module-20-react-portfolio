302f3f9497a1d60f6efd5073ece337e60c0767b7
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-19 15:18:26 -0400

    Update Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Project from '../components/Project';
 
 function Portfolio() {
-  return null;
+  return (
+    <section>
+      <h2>Portfolio</h2>
+      <Project />
+    </section>
+  );
 }
 
 export default Portfolio;
