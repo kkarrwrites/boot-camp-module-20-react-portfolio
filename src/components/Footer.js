0a374b133f500fafe1b9d84caf5e9b6485f71b33
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-20 11:16:40 -0400

    Add rel="noreferrer" to external links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,22 @@ function Footer() {
     <footer>
       <ul>
         <li>
-          <a href="https://github.com/kkarrwrites" target="_blank">
+          <a
+            href="https://github.com/kkarrwrites"
+            target="_blank"
+            rel="noreferrer"
+          >
             GitHub
           </a>
         </li>
         <li>
-          <a href="#" target="_blank">
+          <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
             LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+            Twitter
           </a>
         </li>
       </ul>
