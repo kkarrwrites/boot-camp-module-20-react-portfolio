1a615b378dfec6f68422a1f63307670363a1b5b1
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-20 15:34:31 -0400

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../utils/helpers';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'name') {
+      setName(inputValue);
+    } else if (inputType === 'email') {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleBlurChangeName = () => {
+    if (!name) {
+      setErrorMessage('A name is required.');
+      return;
+    }
+  };
+  const handleBlurChangeEmail = () => {
+    if (!email) {
+      setErrorMessage('A valid email address is required.');
+      return;
+    }
+  };
+  const handleBlurChangeMessage = () => {
+    if (!message) {
+      setErrorMessage('A message is required.');
+      return;
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setErrorMessage('A name is required.');
+      return;
+    }
+    if (!validateEmail(email) || !email) {
+      setErrorMessage('A valid email address is required.');
+      return;
+    }
+    if (!message) {
+      setErrorMessage('A message is required.');
+      return;
+    }
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
     <section>
       <h2>Contact</h2>
@@ -8,28 +68,43 @@ function Contact() {
         <label htmlFor="name" required>
           Name (Required)
         </label>
-        <br />
-        <input id="name" type="text" />
-        <br />
-        <label htmlFor="email">Email (Required)</label>
-        <br />
         <input
-          id="email"
-          /* type="email" */
-          pattern="/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/"
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          onBlur={handleBlurChangeName}
+          type="text"
+          placeholder="Name"
           required
         />
-        <br />
+        <label htmlFor="email" required>
+          Email (Required)
+        </label>
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          onBlur={handleBlurChangeEmail}
+          type="email"
+          placeholder="Email"
+          required
+        />
         <label htmlFor="message" required>
           Message (Required)
         </label>
-        <br />
-        <textarea id="message" type="text" />
-        <br />
-        <button type="submit" value="Submit">
+        <textarea
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          onBlur={handleBlurChangeMessage}
+          type="text"
+          required
+        />
+        <button type="button" onClick={handleFormSubmit}>
           Submit
         </button>
       </form>
+      {errorMessage}
     </section>
   );
 }
