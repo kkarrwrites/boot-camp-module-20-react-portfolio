76ff4c373c988b8e46bf6710d28f6caa3f15f229
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-20 11:17:37 -0400

    Add avatar image

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,21 +4,30 @@ function About() {
   return (
     <section>
       <h2>About</h2>
-      <p>Hello! I'm K. Karr, a copy editor turned frontend web developer.</p>
-      <p>
-        I got my start tinkering with HTML for Expage, GeoCities, and
-        LiveJournal in high school.
-      </p>
-      <p>
-        After collecting a few degrees, living abroad for two years, and
-        cultivating an eye for detail working as a copy editor for a decade, I
-        returned to web development out of a strong desire to learn more about
-        the technologies I use every day.
-      </p>
-      <p>
-        I specialize in HTML, CSS, JavaScript, Node.js, Express.js, React, and
-        MongoDB.
-      </p>
+      <div className="about-container">
+        <div className="about-img">
+          <img src={require('../assets/images/Avatar.jpg')} alt="" />
+        </div>
+        <div className="about-text">
+          <p>
+            Hello! I'm K. Karr, a copy editor turned frontend web developer.
+          </p>
+          <p>
+            I got my start tinkering with HTML for Expage, GeoCities, and
+            LiveJournal in high school.
+          </p>
+          <p>
+            After collecting a few degrees, living abroad for two years, and
+            cultivating an eye for detail working as a copy editor for a decade,
+            I returned to web development out of a strong desire to learn more
+            about the technologies I use every day.
+          </p>
+          <p>
+            I specialize in HTML, CSS, JavaScript, Node.js, Express.js, React,
+            and MongoDB.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
