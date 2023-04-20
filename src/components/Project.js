bab30e8ff1b419d998360adbccc9c5437f418f4f
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-20 11:16:47 -0400

    Add rel="noreferrer" to external links

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,14 +3,19 @@ import React from 'react';
 function Project({ imageUrl, title, gitHubUrl, deploymentUrl }) {
   return (
     <article>
-      <img src={imageUrl} alt={title} />
+      <a href={gitHubUrl} target="_blank" rel="noreferrer">
+        <img src={imageUrl} alt={title} />
+      </a>
       <div>
         <h3>{title}</h3>
         <p>
-          <a href={gitHubUrl} target="_blank">
+          <a href={gitHubUrl} target="_blank" rel="noreferrer">
             GitHub
           </a>{' '}
-          | <a href={deploymentUrl} target="_blank"></a>Deployment
+          |{' '}
+          <a href={deploymentUrl} target="_blank" rel="noreferrer">
+            Deployment
+          </a>
         </p>
       </div>
     </article>
