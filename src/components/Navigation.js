72e5bec50bb852e00ae1bf80066b8f05875ba381
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-20 11:17:01 -0400

    Add navigation active state

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,22 +10,38 @@ function Navigation({ currentPage, handlePageChange }) {
       </h1>
       <ul>
         <li>
-          <a href="/" onClick={() => handlePageChange('About')}>
+          <a
+            href="/"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'active' : 'inactive'}
+          >
             About
           </a>
         </li>
         <li>
-          <a href="#portfolio" onClick={() => handlePageChange('Portfolio')}>
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange('Portfolio')}
+            className={currentPage === 'Portfolio' ? 'active' : 'inactive'}
+          >
             Portfolio
           </a>
         </li>
         <li>
-          <a href="#resume" onClick={() => handlePageChange('Resume')}>
+          <a
+            href="#resume"
+            onClick={() => handlePageChange('Resume')}
+            className={currentPage === 'Resume' ? 'active' : 'inactive'}
+          >
             Resume
           </a>
         </li>
         <li>
-          <a href="#contact" onClick={() => handlePageChange('Contact')}>
+          <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={currentPage === 'Contact' ? 'active' : 'inactive'}
+          >
             Contact
           </a>
         </li>
